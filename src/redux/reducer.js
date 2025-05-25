af7e097e9f87d9e771fe748d55b31e4196e1b58a
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-05-25 14:34:46 +0530

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,1 +1,41 @@
+import {
+  FETCH_IPSUM_REQUEST,
+  FETCH_IPSUM_SUCCESS,
+  FETCH_IPSUM_FAILURE,
+} from './actions';
+
+const initialState = {
+  loading: false,
+  title: '',
+  body: '',
+  error: '',
+};
+
+const ipsumReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_IPSUM_REQUEST:
+      return { ...state, loading: true, error: '' };
+
+    case FETCH_IPSUM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        title: action.payload.title,
+        body: action.payload.body,
+        error: '',
+      };
+
+    case FETCH_IPSUM_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default ipsumReducer;
 
